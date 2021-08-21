4528f9b4e4357192e4851829cdb4b43fabb48047
Devon Hickle <devonhickle@every-day-tech.com>
2021-08-21 08:26:19 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//Insert Scripting for Homepage here
+function buttonFunction() {
+    alert('Success')
+};
+
+
+//Insert Scripting for Photography page here
+
+
+//Insert Scripting for About me/Contact page here
